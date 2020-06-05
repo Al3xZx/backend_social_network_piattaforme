98c17a3f6176e1546b50cd72daa7630dc39851f3
Alessandro <alessandro.molinaro96@gmail.com>
2020-06-05 11:06:46 +0200

    Initial commit prime configurazioni e test

---
+++ src/main/java/com/alessandro_molinaro/social_network/SocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.alessandro_molinaro.social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkApplication.class, args);
+    }
+
+}
