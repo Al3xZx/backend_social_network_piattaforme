ada2821981fd9661352027a28131e26cfeb4356e
Alessandro <alessandro.molinaro96@gmail.com>
2021-07-17 10:04:12 +0200

    .

---
+++ src/main/java/com/alessandro_molinaro/social_network/utils/JwtProvider.java
@@ -0,0 +1,55 @@
+package com.alessandro_molinaro.social_network.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+
+
+import java.util.Map;
+
+/** The type Jwt provider. */
+
+@Slf4j
+public class JwtProvider {
+
+  private static final String issuer = "demo-service";
+
+  /**
+   * Create jwt string.
+   *
+   * @param subject the subject
+   * @param payloadClaims the payload claims
+   * @return the JWT string
+   */
+  public static String createJwt(String subject, Map payloadClaims) {
+    JWTCreator.Builder builder =
+        JWT.create()
+            .withSubject(subject)
+            .withIssuer(issuer)
+            .withIssuedAt(DateTime.now().toDate())
+            .withExpiresAt(DateTime.now().plusMonths(1).toDate());
+
+    if (payloadClaims != null && !payloadClaims.isEmpty()) {
+      for (Map.Entry entry : payloadClaims.entrySet()) {
+        builder.withClaim(entry.getKey(), entry.getValue().toString());
+      }
+    } else {
+      log.warn("You are building a JWT without header claims!");
+    }
+    return builder.sign(Algorithm.HMAC256(SecurityConfig.secret));
+  }
+
+  /**
+   * Verify jwt decoded.
+   *
+   * @param jwt the JWT string
+   * @return the decoded JWT
+   */
+  public static DecodedJWT verifyJwt(String jwt) {
+    return JWT.require(Algorithm.HMAC256(SecurityConfig.secret)).build().verify(jwt);
+  }
+
+  private JwtProvider() {}
+}
